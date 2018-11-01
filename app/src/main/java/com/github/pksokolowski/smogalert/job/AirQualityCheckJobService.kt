05fd1c31418f8eb9b7b163f497f934784164b84f
Paweł Sokołowski <p.k.sokolowski@outlook.com>
2018-11-01 22:47:10 +0100

    fix retry being scheduled in wrong conditions
    Retry airQuality Check was scheduled in some cases despite the API being
    utilized in the current check. Thus the check would not happen again
    as the AirQualityLogsRepository would not permit another request so
    soon.

--- app/src/main/java/com/github/pksokolowski/smogalert/job/AirQualityCheckJobService.kt
+++ app/src/main/java/com/github/pksokolowski/smogalert/job/AirQualityCheckJobService.kt
@@ -3,6 +3,7 @@ package com.github.pksokolowski.smogalert.job
 import android.app.job.JobParameters
 import android.app.job.JobService
 import android.os.AsyncTask
+import com.github.pksokolowski.smogalert.database.AirQualityLog
 import com.github.pksokolowski.smogalert.job.AQLogsComparer.Companion.RESULT_DATA_SHORTAGE_STARTED
 import com.github.pksokolowski.smogalert.job.AQLogsComparer.Companion.RESULT_DEGRADED_PAST_THRESHOLD
 import com.github.pksokolowski.smogalert.job.AQLogsComparer.Companion.RESULT_ERROR_EMERGED
@@ -84,7 +85,11 @@ class AirQualityCheckJobService : JobService() {
                         RESULT_IMPROVED_PAST_THRESHOLD -> notificationHelper.showImprovement()
                         RESULT_OK_AFTER_SHORTAGE_ENDED -> notificationHelper.showAirIsOkAfterShortage()
                         RESULT_DATA_SHORTAGE_STARTED -> notificationHelper.showDataShortage()
-                        RESULT_ERROR_EMERGED -> jobsHelper.scheduleOneTimeRetry(checkParams.sensitivity)
+                        RESULT_ERROR_EMERGED -> if (current?.hasFlag(AirQualityLog.FLAG_USED_API) == true) {
+                            notificationHelper.showError()
+                        } else {
+                            jobsHelper.scheduleOneTimeRetry(checkParams.sensitivity)
+                        }
                         else -> {
                         }
                     }
