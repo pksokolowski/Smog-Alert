2cb0b1df08a7a8d569b673db94e9e833d9729683
Paweł Sokołowski <p.k.sokolowski@outlook.com>
2019-10-09 11:59:12 +0200

    replace a deprecated method
    A deprecated method for checking internet connectivity was replaced with
    a better alternative. The new method is checking whether or not the
    internet access is actually available for data transfer, rather than
    also accepting a state of attempting to establish a connection as a
    "connected" state.

--- app/src/main/java/com/github/pksokolowski/smogalert/utils/InternetConnectionChecker.kt
+++ app/src/main/java/com/github/pksokolowski/smogalert/utils/InternetConnectionChecker.kt
@@ -13,6 +13,6 @@ class InternetConnectionChecker @Inject constructor(private val context: Applica
     fun isConnectionAvailable(): Boolean {
         val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
-        return activeNetwork?.isConnectedOrConnecting == true
+        return activeNetwork?.isConnected == true
     }
 }
